9435dfae2ac0ac94e03ca199adac94ee0f213f75
tuck <tuckerdarby@gmail.com>
2017-04-24 19:48:36 -0600

    inception training

--- training.py
+++ training.py
@@ -1,6 +1,10 @@
 from core import face_train
 from models import squeezenet
+from models import inception_fn
 
 
-face_train(squeezenet.inference, '4_23', max_iter=500, learning_rate=0.000001,
-           people=10, samples=30, image_shape=(None, 32, 32, 3))
+# face_train(squeezenet.inference, '4_23', max_iter=500, learning_rate=0.000001,
+#            people=10, samples=30, image_shape=(None, 32, 32, 3))
+
+face_train(inception_fn.inference, 'inception_tests', max_iter=5, learning_rate=0.001,
+           people=3, samples=5, image_shape=(None, 32, 32, 3))
