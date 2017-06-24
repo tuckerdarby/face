d8764504f6aa04e40d56d78538c0f22cf29ed465
tuck <tuckerdarby@gmail.com>
2017-06-23 23:40:24 -0600

    updated indexer

--- dm_indexer.py
+++ dm_indexer.py
@@ -2,31 +2,47 @@ from __future__ import division
 
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from math import ceil
 from PIL import Image
-from core import face_eval
+from core import embed
 from models.squeezenet import inference
 from constants import *
 
 
-def index_dm(model, name, batch=50):
-    embs = []
+def index_dm(model, run_name, batch=50):
     df = pd.read_csv(DM_DATA_LOC + 'index.csv')
     df = df[df['cropped'] == 1]
     df.to_csv(DM_DATA_LOC + 'cropped_index.csv')
     amount = len(df)
     batches = ceil(amount / batch)
+    embeddings = []
 
-    for i in range(int(batches)):
-        start = i*batch
-        end = (i+1)*batch
-        rows = df.ix[start:end]
-        images = get_images(rows)
-        image_shape = None, 32, 32, 3
-        embeddings = face_eval(model, name, images, image_shape)
-        embs.extend(embeddings)
+    image_shape = (None, 32, 32, 3)
+    inbound = tf.placeholder(tf.float32, image_shape)
+    logits = embed(model, inbound, reuse=True, training=False)
+    restorer = tf.train.Saver()
 
-    emb_df = pd.DataFrame(embs)
+    with tf.Session() as sess:
+        tf.global_variables_initializer()
+        checkpoint = tf.train.get_checkpoint_state(CHECKPOINT_LOC + run_name)
+        if checkpoint and checkpoint.model_checkpoint_path:
+            print 'restoring model'
+            restorer.restore(sess, checkpoint.model_checkpoint_path)
+        else:
+            print 'bad restore model'
+
+        for i in range(int(batches)):
+            print 'batch', i
+            start = i*batch
+            end = (i+1)*batch
+            rows = df.ix[start:end]
+            images = get_images(rows)
+            for k in range(len(images)):
+                feed_dict = {inbound: images}
+                embeddings.append(sess.run(logits, feed_dict))
+
+    emb_df = pd.DataFrame(embeddings)
     emb_df.to_csv(DM_DATA_LOC + 'cropped_embeddings.csv')
 
 
